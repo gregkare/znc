85c1e46fa0446403171eec3696578b856b0b694a
Shane da Silva <shane@dasilva.io>
2014-03-16 19:07:24 -0700

    Use default instead of set
    `default` is the preferred method of setting attribute defaults.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,24 +1,6 @@
-#
-# Cookbook Name:: znc
-# Attributes:: default
-#
-# Copyright (c) 2011-2013, Seth Chisamore
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-set['znc']['data_dir'] = '/etc/znc'
-set['znc']['user'] = 'znc'
-set['znc']['group'] = 'znc'
+default['znc']['data_dir'] = '/etc/znc'
+default['znc']['user'] = 'znc'
+default['znc']['group'] = 'znc'
 
 default['znc']['conf_dir']        = "#{znc['data_dir']}/configs"
 default['znc']['log_dir']         = "#{znc['data_dir']}/moddata/adminlog"
