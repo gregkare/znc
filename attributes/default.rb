2c0760ac0e610281d1559933cc7d379d2f7498e1
Shane da Silva <shane@dasilva.io>
2014-03-16 19:34:24 -0700

    Add more global settings
    Add ability to configure more global settings via attributes.

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,7 +8,14 @@ default['znc']['module_dir']      = "#{znc['data_dir']}/modules"
 default['znc']['users_dir']       = "#{znc['data_dir']}/users"
 default['znc']['users']           = []
 
-default['znc']['port']            = '+7777'
-default['znc']['skin']            = 'dark-clouds'
+default['znc']['anon_ip_limit']   = 10
+default['znc']['connect_delay']   = 5
 default['znc']['max_buffer_size'] = 500
+default['znc']['motd']            = nil
+default['znc']['server_throttle'] = 30
+default['znc']['skin']            = 'dark-clouds'
+default['znc']['status_prefix']   = '*'
+
+default['znc']['port']            = '+7777'
+
 default['znc']['modules']         = %w[webadmin adminlog]
